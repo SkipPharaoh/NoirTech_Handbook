e43d532156296cc398a634fc3aee882312cadccf
skippharaoh <caniggiathompson@gmail.com>
2023-07-16 14:16:09 -0400

    Added Table component to docs

--- packages/@noirtribe-ui/src/lib/docs/utils.tsx
+++ packages/@noirtribe-ui/src/lib/docs/utils.tsx
@@ -43,4 +43,31 @@ const CardData = [
   },
 ];
 
-export { PropData, CardData };
+const TableData = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+];
+
+export { PropData, CardData, TableData };
