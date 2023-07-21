071ae9002b78ffd84adfe5cce7fffbcd449be7f4
skippharaoh <caniggiathompson@gmail.com>
2023-07-20 21:59:47 -0400

    Added Badge components to docs

--- packages/@noirtribe-ui/src/components/core/Badge/Badge.tsx
+++ packages/@noirtribe-ui/src/components/core/Badge/Badge.tsx
@@ -13,7 +13,7 @@ export const badgeVariants = cva(
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
-          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+          "border-transparent bg-red-700 text-red-700-foreground hover:bg-red-700/80",
         outline: "text-foreground",
       },
     },
