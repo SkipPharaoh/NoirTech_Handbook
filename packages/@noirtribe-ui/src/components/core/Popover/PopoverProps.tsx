85f9e23796040cec9b489f8dc64d1fa32fc317db
skippharaoh <caniggiathompson@gmail.com>
2023-08-05 17:40:37 -0400

    Created Popover component, then added Popover to docs & also adjusted Dialog on docs

---
+++ packages/@noirtribe-ui/src/components/core/Popover/PopoverProps.tsx
@@ -0,0 +1,5 @@
+import { Content } from "@radix-ui/react-popover";
+import React from "react";
+
+export interface PopoverContentProps
+  extends React.ComponentPropsWithoutRef<typeof Content> {}
---
+++ packages/@noirtribe-ui/src/components/core/Popover/index.ts
@@ -0,0 +1,4 @@
+import { Popover, PopoverTrigger, PopoverContent } from "./Popover";
+import * as PopoverProps from "./PopoverProps";
+
+export { Popover, PopoverTrigger, PopoverContent, PopoverProps };
